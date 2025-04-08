eb251ffc387f6b2bf9e85b61a84b4fc80b77de82
mmyy0 <mmyy0@192.168.0.160>
2025-04-08 18:54:58 +0900

    4/8 ホテルル8章3の途中まで完了

---
+++ moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
@@ -0,0 +1,33 @@
+package com.example.moattravel.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.moattravel.entity.House;
+import com.example.moattravel.repository.HouseRepository;
+
+@Controller
+@RequestMapping("/admin/houses")
+public class AdminHouseController {
+	
+	private final HouseRepository houseRepository;
+	
+	public AdminHouseController(HouseRepository houseRepository) {
+		this.houseRepository = houseRepository; 
+	}
+	
+	@GetMapping
+	public String index(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable) {
+		Page<House> housePage = houseRepository.findAll(pageable);
+		
+		model.addAttribute("housePage", housePage);
+		
+		return "admin/houses/index";
+	}
+}
